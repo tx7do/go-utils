1c225465de38d7d40953ecc89c52b7b67049e384
tx7do <yanglinbo@gmail.com>
2023-10-25 15:50:55 +0800

    feat: scripts.

---
+++ tag.bat
@@ -0,0 +1,6 @@
+git tag v1.0.0
+git tag bank_card/v1.0.0
+git tag entgo/v1.0.0
+git tag geoip/v1.0.0
+
+git push origin --tags
---
+++ upgrade.bat
@@ -0,0 +1,11 @@
+echo off
+
+::指定起始文件夹
+set DIR="%cd%"
+
+for /R %DIR% /d %%i in (*) do (
+    echo %%i
+    cd %%i
+    go get all
+    go mod tidy
+)
