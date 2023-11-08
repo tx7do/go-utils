d65e7bb92890e56e4caae6ac0192caba4cbb2142
tx7do <yanglinbo@gmail.com>
2023-11-08 18:15:48 +0800

    feat: ent update fields.

---
+++ entgo/update/update.go
@@ -0,0 +1,25 @@
+package update
+
+import (
+	"entgo.io/ent/dialect/sql"
+	"github.com/tx7do/go-utils/stringcase"
+)
+
+func BuildSetNullUpdate(u *sql.UpdateBuilder, fields []string) {
+	if len(fields) > 0 {
+		for _, field := range fields {
+			field = stringcase.ToSnakeCase(field)
+			u.SetNull(field)
+		}
+	}
+}
+
+func BuildSetNullUpdater(fields []string) (error, func(u *sql.UpdateBuilder)) {
+	if len(fields) > 0 {
+		return nil, func(u *sql.UpdateBuilder) {
+			BuildSetNullUpdate(u, fields)
+		}
+	} else {
+		return nil, nil
+	}
+}
---
+++ entgo/update/update_test.go
@@ -0,0 +1,28 @@
+package update
+
+import (
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/sql"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBuildSetNullUpdate(t *testing.T) {
+	t.Run("MySQL_Set2", func(t *testing.T) {
+		s := sql.Dialect(dialect.MySQL).Update("users")
+
+		BuildSetNullUpdate(s, []string{"id", "username"})
+		query, args := s.Query()
+		require.Equal(t, "UPDATE `users` SET `id` = NULL, `username` = NULL", query)
+		require.Empty(t, args)
+
+	})
+	t.Run("PostgreSQL_Set2", func(t *testing.T) {
+		s := sql.Dialect(dialect.Postgres).Update("users")
+
+		BuildSetNullUpdate(s, []string{"id", "username"})
+		query, args := s.Query()
+		require.Equal(t, `UPDATE "users" SET "id" = NULL, "username" = NULL`, query)
+		require.Empty(t, args)
+	})
+}
