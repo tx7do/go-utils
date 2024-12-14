801deb98cd057d4948d99f3cbba17880853c993a
tx7do <yanglinbo@gmail.com>
2024-12-14 13:07:01 +0800

    feat: ent support update json field.

--- entgo/go.mod
+++ entgo/go.mod
@@ -10,13 +10,14 @@ require (
 	github.com/XSAM/otelsql v0.35.0
 	github.com/go-kratos/kratos/v2 v2.8.2
 	github.com/google/uuid v1.6.0
-	github.com/stretchr/testify v1.9.0
+	github.com/stretchr/testify v1.10.0
 	github.com/tx7do/go-utils v1.1.13
-	go.opentelemetry.io/otel v1.32.0
+	go.opentelemetry.io/otel v1.33.0
+	google.golang.org/protobuf v1.35.2
 )
 
 require (
-	ariga.io/atlas v0.28.1 // indirect
+	ariga.io/atlas v0.29.0 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
@@ -27,21 +28,21 @@ require (
 	github.com/go-openapi/inflect v0.21.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.22.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.23.0 // indirect
 	github.com/jhump/protoreflect v1.17.0 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sony/sonyflake v1.2.0 // indirect
-	github.com/zclconf/go-cty v1.15.0 // indirect
-	go.opentelemetry.io/otel/metric v1.32.0 // indirect
-	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	github.com/zclconf/go-cty v1.15.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/metric v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.33.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/mod v0.22.0 // indirect
-	golang.org/x/sync v0.9.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
-	golang.org/x/tools v0.27.0 // indirect
-	google.golang.org/protobuf v1.35.1 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
--- entgo/update/update.go
+++ entgo/update/update.go
@@ -1,8 +1,16 @@
 package entgo
 
 import (
+	"fmt"
+	"strings"
+
 	"entgo.io/ent/dialect/sql"
+
+	"github.com/tx7do/go-utils/fieldmaskutil"
 	"github.com/tx7do/go-utils/stringcase"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 func BuildSetNullUpdate(u *sql.UpdateBuilder, fields []string) {
@@ -14,12 +22,72 @@ func BuildSetNullUpdate(u *sql.UpdateBuilder, fields []string) {
 	}
 }
 
-func BuildSetNullUpdater(fields []string) (error, func(u *sql.UpdateBuilder)) {
-	if len(fields) > 0 {
-		return nil, func(u *sql.UpdateBuilder) {
-			BuildSetNullUpdate(u, fields)
+// BuildSetNullUpdater 构建一个UpdateBuilder，用于清空字段的值
+func BuildSetNullUpdater(fields []string) func(u *sql.UpdateBuilder) {
+	if len(fields) == 0 {
+		return nil
+	}
+
+	return func(u *sql.UpdateBuilder) {
+		BuildSetNullUpdate(u, fields)
+	}
+}
+
+// ExtractJsonFieldKeyValues 提取json字段的键值对
+func ExtractJsonFieldKeyValues(msg proto.Message, paths []string) []string {
+	var keyValues []string
+	rft := msg.ProtoReflect()
+	for _, path := range paths {
+		fd := rft.Descriptor().Fields().ByName(protoreflect.Name(path))
+		if fd == nil {
+			continue
 		}
-	} else {
-		return nil, nil
+		if !rft.Has(fd) {
+			continue
+		}
+
+		keyValues = append(keyValues, fmt.Sprintf("'%s'", stringcase.ToSnakeCase(path)))
+
+		v := rft.Get(fd)
+		switch v.Interface().(type) {
+		case int32, int64, uint32, uint64, float32, float64, bool:
+			keyValues = append(keyValues, fmt.Sprintf("%d", v.Interface()))
+		case string:
+			keyValues = append(keyValues, fmt.Sprintf("'%s'", v.Interface()))
+		}
+	}
+
+	return keyValues
+}
+
+// SetJsonNullFieldUpdateBuilder 设置json字段的空值
+func SetJsonNullFieldUpdateBuilder(fieldName string, msg proto.Message, paths []string) func(u *sql.UpdateBuilder) {
+	nilPaths := fieldmaskutil.NilValuePaths(msg, paths)
+	if len(nilPaths) == 0 {
+		return nil
+	}
+
+	return func(u *sql.UpdateBuilder) {
+		u.Set(fieldName,
+			sql.Expr(
+				fmt.Sprintf("\"%s\" - '{%s}'::text[]", fieldName, strings.Join(nilPaths, ",")),
+			),
+		)
+	}
+}
+
+// SetJsonFieldValueUpdateBuilder 设置json字段的值
+func SetJsonFieldValueUpdateBuilder(fieldName string, msg proto.Message, paths []string) func(u *sql.UpdateBuilder) {
+	keyValues := ExtractJsonFieldKeyValues(msg, paths)
+	if len(keyValues) == 0 {
+		return nil
+	}
+
+	return func(u *sql.UpdateBuilder) {
+		u.Set(fieldName,
+			sql.Expr(
+				fmt.Sprintf("\"%s\" || jsonb_build_object(%s)", fieldName, strings.Join(keyValues, ",")),
+			),
+		)
 	}
 }
