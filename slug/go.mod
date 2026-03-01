51ffd1c58b3ef166f65db89898e2d4026ed27c94
Bobo <yanglinbo@gmail.com>
2026-03-01 19:18:35 +0800

    chore: update testify dependency to v1.11.1 and refactor test logging

--- slug/go.mod
+++ slug/go.mod
@@ -6,7 +6,7 @@ toolchain go1.23.2
 
 require (
 	github.com/gosimple/slug v1.15.0
-	github.com/stretchr/testify v1.10.0
+	github.com/stretchr/testify v1.11.1
 )
 
 require (
@@ -18,5 +18,3 @@ require (
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
-
-replace github.com/tx7do/go-utils => ../
--- slug/slug_test.go
+++ slug/slug_test.go
@@ -1,7 +1,6 @@
 package slug
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gosimple/slug"
@@ -12,33 +11,33 @@ func TestGoSimple(t *testing.T) {
 	// 俄文
 	text := slug.Make("Hellö Wörld хелло ворлд")
 	assert.Equal(t, text, "hello-world-khello-vorld")
-	fmt.Println(text)
+	t.Log(text)
 
 	// 繁体中文
 	someText := slug.Make("影師")
 	assert.Equal(t, someText, "ying-shi")
-	fmt.Println(someText)
+	t.Log(someText)
 
 	// 简体中文
 	cnText := slug.Make("天下太平")
 	assert.Equal(t, cnText, "tian-xia-tai-ping")
-	fmt.Println(cnText)
+	t.Log(cnText)
 
 	// 英文
 	enText := slug.MakeLang("This & that", "en")
 	assert.Equal(t, enText, "this-and-that")
-	fmt.Println(enText)
+	t.Log(enText)
 
 	// 德文
 	deText := slug.MakeLang("Diese & Dass", "de")
 	assert.Equal(t, deText, "diese-und-dass")
-	fmt.Println(deText)
+	t.Log(deText)
 
 	// 保持大小写
 	slug.Lowercase = false
 	deUppercaseText := slug.MakeLang("Diese & Dass", "de")
 	assert.Equal(t, deUppercaseText, "Diese-und-Dass")
-	fmt.Println(deUppercaseText)
+	t.Log(deUppercaseText)
 
 	// 字符替换
 	slug.CustomSub = map[string]string{
@@ -46,43 +45,45 @@ func TestGoSimple(t *testing.T) {
 	}
 	textSub := slug.Make("water is hot")
 	assert.Equal(t, textSub, "sand-is-hot")
-	fmt.Println(textSub)
+	t.Log(textSub)
 }
 
 func TestGenerate(t *testing.T) {
 	// 俄文
 	text := Generate("Hellö Wörld хелло ворлд")
 	assert.Equal(t, text, "hello-world-khello-vorld")
-	fmt.Println(text)
+	t.Log(text)
 
 	// 繁体中文
 	someText := Generate("影師")
 	assert.Equal(t, someText, "ying-shi")
-	fmt.Println(someText)
+	t.Log(someText)
 
 	// 简体中文
 	cnText := Generate("天下太平")
 	assert.Equal(t, cnText, "tian-xia-tai-ping")
-	fmt.Println(cnText)
+	t.Log(cnText)
 
 	// 英文
 	enText := GenerateEnglish("This & that")
 	assert.Equal(t, enText, "this-and-that")
-	fmt.Println(enText)
+	t.Log(enText)
 
 	enText = GenerateCaseSensitive("This & That")
 	assert.Equal(t, enText, "This-and-That")
-	fmt.Println(enText)
+	t.Log(enText)
 
 	// 德文
 	deText := GenerateGerman("Diese & Dass")
 	assert.Equal(t, deText, "diese-und-dass")
-	fmt.Println(deText)
+	t.Log(deText)
 
 	slug.CustomSub = map[string]string{
 		"water": "sand",
 	}
 	textSub := Generate("water is hot")
 	assert.Equal(t, textSub, "sand-is-hot")
-	fmt.Println(textSub)
+	t.Log(textSub)
+
+	t.Logf(Generate("【无标题】"))
 }
