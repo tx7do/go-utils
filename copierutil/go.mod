c25bf87d51564702a4bfabdc6411f1185168828b
Bobo <yanglinbo@gmail.com>
2026-04-21 17:46:17 +0800

    chore: update go-utils to v1.1.37, protobuf to v1.36.11, and bump version to v1.1.37

--- copierutil/go.mod
+++ copierutil/go.mod
@@ -4,7 +4,7 @@ go 1.25.0
 
 require (
 	github.com/jinzhu/copier v0.4.0
-	github.com/tx7do/go-utils v1.1.34
+	github.com/tx7do/go-utils v1.1.37
 )
 
 require (
--- tag.bat
+++ tag.bat
@@ -1,9 +1,9 @@
-git tag v1.1.36
+git tag v1.1.37
 
 git tag bank_card/v1.1.6
 git tag geoip/v1.1.8
 git tag translator/v1.1.3
-git tag copierutil/v0.0.6
+git tag copierutil/v0.0.7
 git tag jwtutil/v0.0.3
 git tag id/v0.0.5
 git tag slug/v0.0.1
